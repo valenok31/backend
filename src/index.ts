40c8e302c9270be254e57e4477710b20a1e1a4fb
Andrey <priv1984@yandex.ru>
2023-01-19 01:24:32 +0700

    setting

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import express  from 'express'
 
 const app = express()
-const port = 3006
+const port = 3000
+
+
 
 app.get('/', (req, res) => {
-    res.send('o my god!!! Andersen!')
+    res.send('o my god! Andersen!')
 })
 app.get('/users', (req, res) => {
-    res.send('this is lessen and write!!!!!!!')
+    res.send('this is lessen and write!')
 })
+
 app.post('/users', (req, res) => {
     res.send('we have creator')
+})
+app.get('/users1', (req, res) => {
+    res.send('this is lessen and write!')
+})
+app.get('/users2', (req, res) => {
+    res.send('this is lessen and write!')
 })
 
 app.listen(port, () => {
